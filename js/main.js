3fa4c50d94b11546a03cc2d75d785e948fa8d598
Laura <laurav.zarco@gmail.com>
2018-07-11 15:48:28 +0200

    rama de js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 'use strict';
+var input = document.querySelector('.js.input');
+var button =document.querySelector('.js-button');
 
-console.log('>> Ready :)');
+function getShowInfo(){
+  fetch('http://api.tvmaze.com/search/shows?q=' + input.value)
+}
+
+
+
+button.addEventListener('click', getShowInfo)
